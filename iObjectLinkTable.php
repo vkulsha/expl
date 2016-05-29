7cfb5e802cd47d4e78f57383a4d6c4181b20c40c
Вячеслав Кульша <C:\Users\\AppData\Roaming\The Bat!>
2016-05-29 21:13:39 +0300

    20160529 iObjectLinkTable

---
+++ iObjectLinkTable.php
@@ -0,0 +1,47 @@
+<table>
+	<tr>
+		<td>
+			<div id="jstree" style="position:absolute; text-align:left; background-color:#fffff0"></div>
+		</td>
+	</tr>
+	<tr>
+		<td align="center" id="labelComment">ObjectLinkTable</td>
+	</tr>
+	<tr>
+		<td class="jsObjectTable" id="divData">
+			<table border=0><!--ИК-->
+				<tr>
+					<td>ПЭИ2</td>
+					<td>
+						<table border=1><!--Объект-->
+							<tr>
+								<td>объект1</td>
+								<td>
+									<table border=1><!--Здания и сооружения-->
+										<tr>
+											<td>адм здание</td>
+											<td>
+												<table border=1><!--Файлы-->
+													<tr>
+														<td>файл1</td>
+													</tr>
+													<tr>
+														<td>файл2</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+
+<script>
+
+</script>
